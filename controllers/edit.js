47b33d22b32f97b5202898f766535911283ffae7
Elaine Chen <elainechen@Elaines-MacBook-Air.local>
2021-04-08 07:20:51 -0400

    cms frontend

--- controllers/edit.js
+++ controllers/edit.js
@@ -17,8 +17,19 @@ drafts = [
     draftBlurb: "This is the blurb of the article what a cool blurb",
     draftDate: "April 4 2021",
     draftAuthor: "Taylor Swift"
+  },
+  {
+    draftTitle: "This is such a cool title it really is!!",
+    draftBlurb: "This is the blurb of the article what a cool blurb",
+    draftDate: "April 4 2021",
+    draftAuthor: "Taylor Swift"
   }
 ]
+
+// NOTE: If you have time
+// Can load different articles based on whether published or drafts selected
+// If you don't have time, can take that part out of the HTML
+
 module.exports = {
     getEdit
 };
